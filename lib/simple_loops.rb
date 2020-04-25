828aa18c573f2d476c5e0fc2f0958d7976ab46e5
JT Skidmore <johntoddskidmore@gmail.com>
2020-04-25 16:38:59 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,2 +1,22 @@
 # Write your methods here
 
+def loop_message_five_times (string)
+  counter = 1
+  while counter < 6 do
+    puts string
+    counter += 1
+  end
+end
+
+def loop_message_n_times (string, n)
+  counter = n
+  while counter > 0 do 
+    puts string
+    counter -= 1
+  end
+end
+
+def output_array (array)
+  counter = 0
+  while array[]
+end
